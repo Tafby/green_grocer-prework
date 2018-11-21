6506e68165a23446e7e47e914848c4eefbdb9e0c
Tiffany Carter <tafby88@gmail.com>
2018-11-21 05:38:29 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  total = nil
+  total = 0
   cart_hash = consolidate_cart(cart)
     if cart_hash.length >= 1
     coupons = apply_coupons(cart_hash, coupons)
