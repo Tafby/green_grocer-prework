c7eb6af489a594b42d4510bc603ed4a02de39d4d
Tiffany Carter <tafby88@gmail.com>
2018-11-21 05:38:37 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,7 @@ def checkout(cart, coupons)
     if cart_hash.length >= 1
     coupons = apply_coupons(cart_hash, coupons)
       apply_clearance(coupons).each do |item, details|
-        total.to_i += details[:price]
+        total += details[:price]
         return total
     end
   end
